c9b5cb3428682cb2dc98510f639ae7792cf5d8fb
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-06-30 19:42:47 -0400

    fixing deployment

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import InquirerForm from "./inquiryForm/InquiryForm";
+import InquirerForm from "./inquiryForm/InquiryForm.js";
 import Tours from "./tour/Tour";
 import Videos from "./Videos/Video";
 import "bootstrap/dist/css/bootstrap.min.css";
